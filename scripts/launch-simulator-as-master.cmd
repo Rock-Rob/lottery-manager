fcb304b0f165eb9d6317454cc49b6f8acbcc29de
Rock-Rob <robrock80@yahoo.it>
2024-03-06 05:28:01 +0100

    Removed unuseful file

--- scripts/launch-simulator-as-master.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-title SE Lottery simulators (master mode)
-
-set forceMaster=true
-
-call "%~dp0simulator-launcher.cmd"
--- scripts/launch-simulator-as-slave.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-title SE Lottery simulators (slave mode)
-
-set forceSlave=true
-
-call "%~dp0simulator-launcher.cmd"
